1f8069f5a7a7dff8e6697d811613be1a0de7af70
Mara Green <maragreen42@gmail.com>
2022-11-14 20:00:30 -0800

    Added Get By Id request in Postman + updated urls.py with new path

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Product
 from.serializers import ProductSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET', 'POST'])
 def product_list(request):
@@ -18,4 +19,9 @@ def product_list(request):
         serializer.save()
         return Response(serializer.data,status= status.HTTP_201_CREATED)
 
-
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)

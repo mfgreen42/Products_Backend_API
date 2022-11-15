656b18ddfeebfa1a710833ccd4dafbf197155e96
Mara Green <maragreen42@gmail.com>
2022-11-14 19:40:07 -0800

    Added POST function to views

--- products/views.py
+++ products/views.py
@@ -3,10 +3,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from.serializers import ProductSerializer
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
 
-    return Response(serializer.data)
+    if request.method =='GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status= status.HTTP_201_CREATED)
+
+
--- products_project/settings.py
+++ products_project/settings.py
@@ -70,7 +70,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
-
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
